51ed44a470e87aca59be492fd90f563eb1c2ef42
Nahian Ahmed <nahianahmedcse@gmail.com>
2018-03-14 03:35:45 +0600

    ading sing up

---
+++ usersingup.php
@@ -0,0 +1,57 @@
+<!DOCTYPE>
+<html>
+<body>
+<head>
+<title>Welcome To EPR System</title>
+
+<link rel="stylesheet" href="usersingup.css" media="all" />
+</head>
+
+<div class="c1">
+<div class="logo">
+<div class="l1">
+<strong>Electronic Patient Record System</strong>
+<p>Emergency no:...........</p>
+</div>
+<div class="l2">
+
+<img src="2.jpg" width="60" height="60">
+<div class="menuber">
+
+
+<ul id="menu">
+
+   <li><a href="index.php">HOME</a></li>
+  <li><a href="#news">SERVICES</a></li>
+  <li><a href="login.php">LOG IN</a></li>
+<li><a href="usersingup.php">SING UP</a></li>
+
+  <li><a href="#about">ABOUT</a></li>
+  <li><a href="#about">CONTACT</a></li>
+    
+  
+</ul>
+</div>
+
+</div>
+
+</div>
+
+<div class="contentarea">
+<div class="h1">
+<h1>Sing Up</h1>
+</div>
+
+<div class="user">
+
+<a href="doctor_signup.php"><button>Doctor</button></a>
+<a href="Regipharmacy.php"><button>Pharmacy</button></a>
+<a href="Regipatient.php"><button>Patient</button></a>
+
+</div>
+</div>
+</div>
+
+</body>
+
+</html>
