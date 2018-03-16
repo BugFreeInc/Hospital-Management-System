4733169a1be3bf1cba4b7bbfd3011239516847ba
Monsur Ahmed Shafiq <monsurahmedshafiq@gmail.com>
2018-03-17 00:26:25 +0600

    Adding bootstrap
    Adding bootstrap

---
+++ doctorpage_3.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css"  crossorigin="anonymous">
+
+    <title>Doctor Page</title>
+  </head>
+  <body>
+   
+<div class=container>
+
+<div class="manuber">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    
+
+
+    <!---<a class="navbar-brand" href="#">Hidden brand</a>-->
+
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="doctorpage_1.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="doctorpage_2.php">Genaral Prescribe</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="doctorpage_3.php">Patient History</a>
+      </li>
+      
+    </ul>
+    
+    <button type="button" class="btn btn-light" name="">Log Out</button>
+  </div>
+</div><!---mmanuber End-->
+<div class="Doctor_Name">
+  <h1>Dr.abcd </h1>
+<p>MBBS,MD(Medicin)</p>
+<p>Govt.Reg:1234</p>
+<p>Medical offiser,SMC</p>
+</div>
+<div class="c2">
+
+<div class="history">
+  <p>Patient History</p>
+</div>
+
+ 
+</div>
+
+<div class="slideber">
+  <nav class="navbar navbar-light bg-light">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
+</div><!---slide_ber End-->
+
+</div>    <!---container End-->
+
+</nav>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
