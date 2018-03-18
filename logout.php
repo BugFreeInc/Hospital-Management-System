1925df01d48480fd1719a74dd5c41c3b7e67b3b0
Nahian Ahmed <nahianahmedcse@gmail.com>
2018-03-19 03:17:00 +0600

    update logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,15 @@
    $_SESSION["Doctor_id"] = "";
    $_SESSION["Doctor_Qualification"] = "";
    $_SESSION["Doctor_Name"] = "";
+
+   $_SESSION["p_nid"] ="";
+   $_SESSION["p_name"] ="";
+
+   $_SESSION["pharmacy_id"] = "";
+   $_SESSION["pharmacy_name"] = "";
+   $_SESSION["pharmacy_owner"] = "";
+
+
   header("refresh:0;url=login.php");
 
 
